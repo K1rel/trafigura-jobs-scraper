4eb36690531336f1b0697934dd5ae2f2cbf192c4
k1rel <kiril.gerasimovski@gmail.com>
2024-11-11 19:44:04 +0100

    added scraping for all paginated data

--- trafigura_jobs/spiders/job_positions.py
+++ trafigura_jobs/spiders/job_positions.py
@@ -1,6 +1,14 @@
+import random
 from typing import Iterable
 import scrapy
 from scrapy_playwright.page import PageMethod
+
+
+async def scroll_page(page) -> str:
+    await page.wait_for_selector(selector="section[data-automation-id='jobResults'] ul[role='list'] > li:first-child")
+    await page.evaluate("window.scrollBy(0, document.body.scrollHeight)")
+    await page.wait_for_selector(selector="section[data-automation-id='jobResults'] ul[role='list'] > li:last-child")
+    return page.url
 
 class JobPositionsSpider(scrapy.Spider):
     name = 'job_positions'
@@ -9,18 +17,80 @@ class JobPositionsSpider(scrapy.Spider):
     def start_requests(self):
         yield scrapy.Request(self.start_urls[0], meta=dict(
             playwright=True,
-            playwright_include_page=True, 
-            playwright_page_methods=[
-                PageMethod("wait_for_selector", "section[data-automation-id='jobResults'] ul[role='list']"),
-                
-            ]
-        ))
+            playwright_include_page=True,  
+            playwright_page_methods= [PageMethod(scroll_page)]
+         
+            )
+        )
 
     async def parse(self, response):
-        # page = response.meta["playwright_page"]
-        # await page.screenshot(path="example.png", full_page=True)
+        page = response.meta["playwright_page"]
+        while True:
+
+            for job in response.css("section[data-automation-id='jobResults'] ul[role='list'] > li"):
+                yield {
+                    'title': job.css("a[data-automation-id='jobTitle']::text").get()
+                }
+
+            next_button_selector = "button[data-uxi-element-id='next']"
+            next_button = await page.query_selector(next_button_selector)
+
+            if not next_button:
+                self.log("No more pages to scrape.")
+                break
+
+            await next_button.click()
+            await page.wait_for_selector("section[data-automation-id='jobResults'] ul[role='list'] > li")
+
+            
+            content = await page.content()
+            response = scrapy.http.HtmlResponse(
+                url=page.url,
+                body=content,
+                encoding='utf-8',
+                request=response.request
+            )
+
+
+        await page.close()
+
+
+        
+
+
+# page = response.meta["playwright_page"]
+        # while True:
+
+        #     for job in response.css("section[data-automation-id='jobResults'] ul[role='list'] > li"):
+        #         yield {
+        #             'title': job.css("a[data-automation-id='jobTitle']::text").get()
+        #         }
+
+        #     next_button_selector = "button[data-uxi-element-id='next']"
+        #     next_button = await page.query_selector(next_button_selector)
+
+        #     if not next_button:
+        #         self.log("No more pages to scrape.")
+        #         break
+
+        #     await next_button.click()
+        #     await page.wait_for_selector("section[data-automation-id='jobResults'] ul[role='list'] > li")
+
+            
+        #     content = await page.content()
+        #     response = scrapy.http.HtmlResponse(
+        #         url=page.url,
+        #         body=content,
+        #         encoding='utf-8',
+        #         request=response.request
+        #     )
+
+
         # await page.close()
-        for job in response.css("section[data-automation-id='jobResults'] ul[role='list'] > li"):
-            yield {
-                'title': job.css("a[data-automation-id='jobTitle']::text").get()
-            }
+
+
+
+        
+       
+
+
